4e3ef08c733fb3b266fcd06482099d61b8ec27e8
hafidzabudi27-oss <hafidzabudi27@gmail.com>
2026-06-09 22:24:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
+// Nama tamu otomatis
 
+const params = new URLSearchParams(window.location.search);
+
+const tamu = params.get("to");
+
+if(tamu){
+document.getElementById("tamu").innerHTML =
+decodeURIComponent(tamu);
+}
+
+// Tombol buka undangan
+
+function bukaUndangan(){
+
+document
+.getElementById("undangan")
+.scrollIntoView({
+behavior:"smooth"
+});
+
+}
+
+// Countdown
+
+const target =
+new Date("June 14, 2026 15:00:00").getTime();
+
+setInterval(()=>{
+
+const now = new Date().getTime();
+
+const distance = target - now;
+
+document.getElementById("hari").innerHTML =
+Math.floor(distance/(1000*60*60*24));
+
+document.getElementById("jam").innerHTML =
+Math.floor(
+(distance%(1000*60*60*24))
+/
+(1000*60*60)
+);
+
+document.getElementById("menit").innerHTML =
+Math.floor(
+(distance%(1000*60*60))
+/
+(1000*60)
+);
+
+document.getElementById("detik").innerHTML =
+Math.floor(
+(distance%(1000*60))
+/
+1000
+);
+
+},1000);
